b9451321ffdfeb5bd2ea9ec0dca77483dea06a90
alstep07 <sashastepanenko2007@gmail.com>
2025-04-15 17:34:11 +0300

    feat: hero banner

--- src/widgets/common/cta-banner/index.tsx
+++ src/widgets/common/cta-banner/index.tsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
 import { Logo } from "@/shared/icons";
 import { Button } from "@/shared/ui";
 
 export const CTABanner: React.FC = () => {
   return (
-    <aside className="gradient-primary rounded-lg">
-      <div className="flex flex-col gap-4 md:gap-6 p-4 md:p-6">
+    <aside className="relative gradient-primary rounded-lg flex">
+      <div className="md:max-w-[70%] flex flex-col gap-4 md:gap-6 p-4 md:p-6">
         <Logo />
         <div className="flex flex-col gap-2">
           <h3 className="text-xl font-[800] md:font-[600] text-primary-400">
@@ -15,8 +16,17 @@ export const CTABanner: React.FC = () => {
           </p>
         </div>
         <div className="md:self-start flex flex-col gap-4 md:gap-6">
-          <Button>Subscribe</Button>
+          <Button>Try For Free</Button>
         </div>
+      </div>
+      <div className="hidden md:block absolute -right-[15px] -top-[9px] w-[336px] max-w-[35%] aspect-[1.29]">
+        <Image
+          src="/common/cta_image.png"
+          alt="CTA Banner"
+          fill
+          className="object-cover transition-transform duration-300 group-hover:scale-110 will-change-transform"
+          quality={75}
+        />
       </div>
     </aside>
   );
